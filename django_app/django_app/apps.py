7a58de0b373e098b2ede5fea0536b64ab71c6448
Ubuntu <cse291@distributedsystems.lk0vk3r44atuzlcovoclqaxtzg.dx.internal.cloudapp.net>
2017-09-19 13:17:54 +0000

    migtr

---
+++ django_app/django_app/apps.py
@@ -0,0 +1,4 @@
+from django.apps import AppConfig
+class myAppNameConfig(AppConfig):
+    name = 'django_app'
+    verbose_name = 'A Much Better Name'
--- django_app/django_app/models.py
+++ django_app/django_app/models.py
@@ -2,11 +2,11 @@ from django.db import models
 
 class words(models.Model):
     
-    word = models.CharField()
-    synonyms = models.CharField()
-    description=models.CharField()
-    group=models.CharField()
-    sentence=models.CharField()
+    word = models.CharField(max_length=200)
+    synonyms = models.CharField(max_length=200)
+    description=models.CharField(max_length=200)
+    group=models.CharField(max_length=200)
+    sentence=models.CharField(max_length=500)
     def create(self,word,synonyms,descriptn,sentence,group):
         self.word=word
         self.synonyms=synonyms
